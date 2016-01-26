14db2b2e9555daa9129a0911e5dbed152b4b5988
julio <caicedo.julio@gmail.com>
2016-01-25 22:58:10 -0430

    tablelayout

--- MyApplication/app/src/main/java/com/example/berraco/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/berraco/myapplication/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.berraco.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1, tv2, tv3;
+    private EditText edit1;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bt1= (Button)findViewById(R.id.b1);
+        tv1=(TextView)findViewById(R.id.tv1);
+        tv2=(TextView)findViewById(R.id.tv2);
+        tv3=(TextView)findViewById(R.id.tv3);
+
+        edit1=(EditText)findViewById(R.id.edit1);
+        btn1=(Button)findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String texto= edit1.getText().toString();
+
+                tv1.setText(texto);
+                tv2.append(texto);
+                tv3.setText(texto);
+            }
+        });
+
+
     }
 }
